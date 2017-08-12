aa0df229432b5f62651aaaf081a89b118204a37d
JOSE RAMIREZ <hoyeay5@gmail.com>
2017-08-12 09:29:24 -0500

    Added static pages testing and updated routes and view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
+# Controller for the web apps static pages
 class StaticPagesController < ApplicationController
   def home
+  end
+  def about
+  end
+  def contact
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,29 @@
 require 'test_helper'
-
+# Static Pages Controller Testing
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
+  def setup
+    @base_title = 'Presstee' # set var
   end
-
+  test 'should get root' do
+    get root_url # get root web page
+    assert_response :success # check success
+    assert_select 'title', "#{@base_title} | Home"
+  end
+=begin Commented out due to home now being the above root test
+  test 'should get home' do
+    get static_pages_home_url # get the web page
+    assert_response :success # check success
+    assert_select 'title', "#{@base_title} | Home" # check web page title
+  end
+=end
+  test 'should get about' do
+    get static_pages_about_url # get the web page
+    assert_response :success # check success
+    assert_select 'title', "#{@base_title} | About"
+  end
+  test 'should get contact' do
+    get static_pages_contact_url # get the web page
+      assert_response :success # check success
+    assert_select 'title', "#{@base_title} | Contact"
+  end
 end
